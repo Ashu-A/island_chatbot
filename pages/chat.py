a4aadd85ec3dfafbbdb1f0c1f543bf710b20a132
A Ranjan <68651277+Ashu-A@users.noreply.github.com>
2024-04-19 21:42:36 +0100

    Update chat.py

--- pages/chat.py
+++ pages/chat.py
@@ -1,4 +1,3 @@
-# import libraries
 import streamlit as st
 import pandas as pd
 from pandasai.llm.openai import OpenAI
@@ -8,9 +7,11 @@ from specklepy.api.client import SpeckleClient
 from specklepy.api import operations
 from dotenv import load_dotenv
 import os
+
 # Load the .env file
 load_dotenv()
-# functions
+
+# Functions
 def chat_speckle(df, prompt):
     openai_api_token = os.getenv('OPENAI_API_TOKEN')
     llm = OpenAI(api_token=openai_api_token)
@@ -18,18 +19,11 @@ def chat_speckle(df, prompt):
     result = df.chat(prompt)
     return result
 
-
-# get parameter names
 def get_parameter_names(commit_data, selected_category):
     parameters = commit_data[selected_category][0]["parameters"].get_dynamic_member_names()
-    parameters_names = []
-    for parameter in parameters:
-        parameters_names.append(commit_data[selected_category][0]["parameters"][parameter]["name"])
-    parameters_names = sorted(parameters_names)
-    return parameters_names
+    parameters_names = [commit_data[selected_category][0]["parameters"][parameter]["name"] for parameter in parameters]
+    return sorted(parameters_names)
 
-
-# get parameter value by parameter name
 def get_parameter_by_name(element, parameter_name, dict):
     for parameter in parameters:
         key = element["parameters"][parameter]["name"]
@@ -37,72 +31,72 @@ def get_parameter_by_name(element, parameter_name, dict):
             dict[key] = element["parameters"][parameter]["value"]
     return dict
 
-
-# containers 📦
-
+# Containers
 header = st.container()
 input = st.container()
 data = st.container()
 
-# header
+# Header
 with header:
     st.title('chatSpeckle 🗣️')
     st.info('This web app allows you to chat with your AEC data using Speckle and OpenAI')
 
-# inputs
+# Inputs
 with input:
     st.subheader('Inputs 📁')
     commit_url = st.text_input('Commit URL', "https://speckle.xyz/streams/06564bda95/commits/f308ed526e")
 
-# wrapper
+# Wrapper
 wrapper = StreamWrapper(commit_url)
-# client
+# Client
 client = wrapper.get_client()
-# trasnport
+# Transport
 transport = wrapper.get_transport()
 
-# get speckle commit
-commit = client.commit.get(wrapper.stream_id, wrapper.commit_id)
-# get object id from commit
-obj_id = commit.referencedObject
-# receive objects from commit
-commit_data = operations.receive(obj_id, transport)
+# Get Speckle commit
+try:
+    commit = client.commit.get(wrapper.stream_id, wrapper.commit_id)
+    obj_id = commit.referencedObject
+    commit_data = operations.receive(obj_id, transport)
+except Exception as e:
+    st.error(f"Error retrieving commit data: {e}")
+    commit_data = None
 
-with input:
-    selected_category = st.selectbox("Select category", commit_data.get_dynamic_member_names())
+if commit_data:
+    with input:
+        selected_category = st.selectbox("Select category", commit_data.get_dynamic_member_names())
 
-# parameters
-parameters = commit_data[selected_category][0]["parameters"].get_dynamic_member_names()
+    # Parameters
+    parameters = commit_data[selected_category][0]["parameters"].get_dynamic_member_names()
 
-with input:
-    form = st.form("parameter_input")
-    with form:
-        selected_parameters = st.multiselect("Select Parameters", get_parameter_names(commit_data, selected_category))
-        run_button = st.form_submit_button('RUN')
+    with input:
+        form = st.form("parameter_input")
+        with form:
+            selected_parameters = st.multiselect("Select Parameters", get_parameter_names(commit_data, selected_category))
+            run_button = st.form_submit_button('RUN')
 
-category_elements = commit_data[selected_category]
+    category_elements = commit_data[selected_category]
 
-with data:
-    st.subheader("Data 📚")
-    result_data = []
-    for element in category_elements:
-        dict = {}
-        for s_param in selected_parameters:
-            get_parameter_by_name(element, s_param, dict)
-        result_data.append(dict)
-    result_DF = pd.DataFrame.from_dict(result_data)
+    with data:
+        st.subheader("Data 📚")
+        result_data = []
+        for element in category_elements:
+            data_dict = {}
+            for selected_param in selected_parameters:
+                get_parameter_by_name(element, selected_param, data_dict)
+            result_data.append(data_dict)
+        result_DF = pd.DataFrame.from_dict(result_data)
 
-    # show dataframe and add chatSpeckle feature
-    col1, col2 = st.columns([1, 1])
-    with col1:
-        result = st.dataframe(result_DF)
+        # Show dataframe and add chatSpeckle feature
+        col1, col2 = st.columns([1, 1])
+        with col1:
+            st.dataframe(result_DF)
 
-    with col2:
-        st.info("⬇️chatSpeckle⬇️")
-        OPENAI_API_KEY = st.text_input('OpenAI key', "sk-...vDlY")
+        with col2:
+            st.info("⬇️chatSpeckle⬇️")
+            openai_api_key = st.text_input('OpenAI key', "sk-...vDlY")
 
-        input_text = st.text_area('Enter your query')
-        if input_text is not None:
+            input_text = st.text_area('Enter your query')
             if st.button("Send"):
                 st.info('Your query:' + input_text)
                 result = chat_speckle(result_DF, input_text)
