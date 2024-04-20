e9fa23b6cb99e2b41b3efda725ee5f24ec53f3b0
A Ranjan <68651277+Ashu-A@users.noreply.github.com>
2024-04-20 07:03:30 +0100

    Update data.py

--- pages/data.py
+++ pages/data.py
@@ -105,7 +105,7 @@ with data_extraction:
         for commit in commits:
             if getattr(commit, "branchName", None) == bName:
                 obj_id = commit.referencedObject
-                commit_data = operations.receive(obj_id, remote_transport=transport)
+                commit_data = operations.receive(obj_id)
                 if commit_data:
                     break
 
