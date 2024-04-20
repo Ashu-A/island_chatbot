25e2ce9bf59678aff2d40007b6ee009ae9c58284
A Ranjan <68651277+Ashu-A@users.noreply.github.com>
2024-04-20 05:40:25 +0100

    Update powerbi.py

--- pages/powerbi.py
+++ pages/powerbi.py
@@ -1,4 +1,12 @@
 import streamlit as st
+st.set_page_config(
+    page_title="Island Chatbot",
+    page_icon="🏝️",
+)
+header = st.container()
+with header:
+    st.title('Island Chatbot')
+    st.info('Hi I am ..... and I am developed by team Island to extract and interact with the data from the revit model.')
 
 def embed_powerbi_dashboard(embed_link):
     if embed_link.startswith("<iframe"):
@@ -7,7 +15,7 @@ def embed_powerbi_dashboard(embed_link):
         st.write("Please enter a valid Power BI embed link.")
 
 def main():
-    st.title('Power BI Dashboard Integration with Streamlit')
+    st.title('Power BI Dashboard')
     st.write("Enter your Power BI embed link below:")
     embed_link = st.text_input("Power BI Embed Link", value='<iframe title="Island(The Body)" width="800" height="600" src="https://app.powerbi.com/view?r=eyJrIjoiNzgzOGY4NmMtNzEyYi00NzE5LWE4NDUtOGZmZjZmZGM4MGM0IiwidCI6IjgwNjBmMmQ0LTI3NTMtNGQ2OS04NTJhLTc0NWZiYTRiNTZlOCJ9" frameborder="0" allowFullScreen="true"></iframe>', help="Paste your Power BI embed link here.")
     st.write("Here's your Power BI dashboard:")
