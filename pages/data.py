5bf4b21eedded508515edb4c8646f4d8c853d53f
A Ranjan <68651277+Ashu-A@users.noreply.github.com>
2024-04-20 05:42:52 +0100

    Update data.py

--- pages/data.py
+++ pages/data.py
@@ -1,1 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Island Chatbot",
+    page_icon="🏝️",
+)
+header = st.container()
+with header:
+    st.title('Island Chatbot')
+    st.info('Page under development')
 
