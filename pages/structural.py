b3c4e6ccc9a4f4e22c067b24b56f6564306fb03c
A Ranjan <68651277+Ashu-A@users.noreply.github.com>
2024-05-04 22:56:18 -0700

    Update structural.py

--- pages/structural.py
+++ pages/structural.py
@@ -6,7 +6,7 @@ st.set_page_config(
 header = st.container()
 with header:
     st.title('Island Chatbot')
-    st.info('Hi I am ..... and I am developed by team Island to extract and interact with the data from the revit model.')
+    st.info('Hi I am SEPI a virtual assistant for Island Team')
 
 def strength_check():
     st.subheader("Strength Check")
